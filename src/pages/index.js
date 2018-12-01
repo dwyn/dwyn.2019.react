491d8be4ac996037825b714fbe084d66ab108138
dwieb <dwayne.harmon@gmail.com>
2018-12-01 15:16:19 -0600

    Why do we bother mentioning 'first commit'?

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello humans!</h1>
+  </Layout>
+)
+
+export default IndexPage
